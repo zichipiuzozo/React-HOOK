dfd065e9ae0d694a550fca42eb5e9d1b04eb9821
Albert Nguyen <tuananhbg19797@gmail.com>
2022-07-01 17:02:27 +0700

    ssss

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Nav from "./views/Nav";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  let obj = { name: "TA", class: "10a1" };
+  let [name, setName] = useState("Tuan Anh");
+
+  const handleEventClick = event => {
+    console.log("click me", name);
+  };
   return (
     <div className="App">
+      <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello world by Tuan Anh</h1>
+        <h1>
+          Hello world by {name} in {obj.class}
+        </h1>
+        {/* <input
+          type="text"
+          defaultValue="Tuan Anh"
+          onClick={event => handleEventClick(event)}
+        /> */}
+        <button
+          type="button"
+          onClick={() => {
+            handleEventClick();
+          }}
+        >
+          click me
+        </button>
       </header>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/views/Nav.js
@@ -0,0 +1,15 @@
+import "../views/Nav.scss";
+const Nav = () => {
+  return (
+    <div className="topnav">
+      <a className="active" href="#home">
+        Home
+      </a>
+      <a href="#news">News</a>
+      <a href="#contact">Contact</a>
+      <a href="#about">About</a>
+    </div>
+  );
+};
+
+export default Nav;
